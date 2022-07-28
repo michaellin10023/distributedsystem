8cadf4edb25785372a6d6b1ab18f213da119a444
Shi-Yu Lin <46772792+michaellin10023@users.noreply.github.com>
2022-07-28 11:58:20 -0700

    add fraud service

--- customer/src/main/java/com/michael/customer/CustomerService.java
+++ customer/src/main/java/com/michael/customer/CustomerService.java
@@ -1,10 +1,15 @@
 package com.michael.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
 
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
             .firstName(request.firstName())
@@ -16,5 +21,15 @@ public record CustomerService(CustomerRepository customerRepository) {
         // todo: check if email not taken
         customerRepository.save(customer);
 
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+            "http://localhost:8081/api/v1/fraud-check/{customerId}",
+            FraudCheckResponse.class,
+            customer.getId()
+        );
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
+        // todo: send notification
     }
 }
---
+++ customer/src/main/java/com/michael/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.michael.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
