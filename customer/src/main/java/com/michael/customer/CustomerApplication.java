394793b4085f6b6a1b99376e8cbba0685deca2fe
Shi-Yu Lin <46772792+michaellin10023@users.noreply.github.com>
2022-08-21 16:43:24 -0700

    deploying microservices to k8s

--- customer/src/main/java/com/michael/customer/CustomerApplication.java
+++ customer/src/main/java/com/michael/customer/CustomerApplication.java
@@ -1,12 +1,11 @@
 package com.michael.customer;
 
-import com.michael.amqp.RabbitMQMessageProducer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
     scanBasePackages = {
@@ -18,6 +17,9 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients(
     basePackages = "com.michael.clients"
 )
+@PropertySources({
+    @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
